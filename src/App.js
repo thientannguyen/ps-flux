99827aa38cc24de7684ed9b797bfa7da9e3d147c
thientannguyen <thientan0206@gmail.com>
2020-02-15 02:12:33 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is Tan Nguyen\'s website.
+          This is Tan Nguyen's website.
         </p>
       </header>
     </div>
